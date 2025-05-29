ed82a7f99123d5786f26bbadca409441e987dcdb
Rohan <kajalsantra816@gmail.com>
2025-05-29 18:15:04 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sundown-studio-clone-using-react/',
+  base: '/Sundown-Studio-Clone-Using-React/',
   plugins: [react()],
 })
